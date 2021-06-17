c1cbd54ddfc5e80cc2fe9d9f5c237d620a365e6e
zackinfosbd <zackinfosbd@gmail.com>
2021-06-17 16:32:06 +0100

    sixth commit

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
-const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+// short-circuit evaluation
+// const ShortCircuit = () => {
+//   const [text, setText] = useState("");
+//   const firstValue = text || "hello world";
+//   const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+//   return (
+//     <>
+//       {/* first */}
+//       {/* {if(){console.log('hello world')}} */}
+//       {/* second */}
+//       {/* <h1>{firstValue}</h1>
+//       <h1>value: {secondValue}</h1> */}
+//       {/* third */}
+//       <h1>{text || "john doe"}</h1>
+//       {text && <h1>hello world</h1>}
+//       {!text && <h1>hello world</h1>}
+//     </>
+//   );
+// };
+
+/*
+* Remember!!!
+    // the component body need to return a value expl-(JSX),and if we set up just
+    if statement with some expression wont work because if does not return
+    any thing implicitly,thats why we need to work with circuit short
+*/
+
+// ternary operator
+const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  return (
+    <>
+      <h1>{text || "john doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {text && <h1>hello world</h1>}
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
+
+/*
+  * the difference between logic operators (short-circuit operators) and 
+ternary operator is that the ternary operator is more flexible than logical 
+operator because it gives two possible values 
+
+* on the level of the callback function we always set within useState(setValue)
+ the oposit value of the state value */
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,43 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  });
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h1>window</h1>
+      <h2>size: {size}PX</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
+
+/*
+  * we have said before that using blank dependency list will save us from
+infinit loops cause by the event listened on the window object;
+and we have said also that infinit loop will cause leaking in the memory
+
+          // ## but here empty dependency list did not help us !!! why??##
+          because now we are toggling the component
+*/
