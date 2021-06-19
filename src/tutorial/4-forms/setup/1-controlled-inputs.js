9fe9be6411da86aa7a32f572c99683a58299f029
zackinfosbd <zackinfosbd@gmail.com>
2021-06-19 21:23:41 +0100

    form second commit

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,9 +6,22 @@ import React, { useState } from "react";
 // value, onChange
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    if (firstName && email) {
+      // console.log("submit the form");
+      const person = { id: new Date().getTime().toString(), firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("empty values");
+    }
   };
   return (
     <>
@@ -21,6 +34,8 @@ const ControlledInputs = () => {
               id="firstName"
               name="firstName"
               placeholder="first name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </div>
           <div className="form-control">
@@ -30,13 +45,36 @@ const ControlledInputs = () => {
               id="email"
               name="email"
               placeholder="email@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <button type="submit">add person</button>
         </form>
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div key={id} className="item">
+              <h4>{firstName} </h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
     </>
   );
 };
 
 export default ControlledInputs;
+
+/*
+  * in React we can submit the form in two ways:
+  // onSubmit on the level of the form 
+  // submit the form on the level of the button of type="submit" and 
+  the callback function */
+
+/*
+  // connect the inputs to state values: 
+  value = {stateValue}
+  // set the onChange 
+  onchange = {(e)=> setValue(e.target.value)*/
