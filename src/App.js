34f931e707d94b65b7d7aa5113060038e403ca20
zackinfosbd <zackinfosbd@gmail.com>
2021-06-22 21:40:32 +0100

    last props drilling commit message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import SetUp from "./tutorial/6-useReducer/setup/index";
+import SetUp from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 function App() {
   return (
     <div className="container">
--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <>
+      <section>
+        <h3>prop drilling</h3>
+        <List people={people} removePerson={removePerson} />
+      </section>
+    </>
+  );
+};
+
+const List = ({ people, removePerson }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const SinglePerson = ({ id, name, removePerson }) => {
+  return (
+    <div className="item">
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>remove</button>
+    </div>
+  );
 };
 
 export default PropDrilling;
+
+/*
+  *  prop drilling is not an official term. However, it is somewhat 
+of the side effect when you have multiple components and then you have the
+big component tree, and then you need to start passing some state from the top
+component all the way to the bottom of your componentry.
+
+* why we will cover prop drilling first is because in a next tutorial chapter 
+in the useContext, we'll see how use context fixes that.
+And more specifically, will we take a look at the context API that is
+designed for that and then useContext hook is the new way how we can access
+that context?
+ */
