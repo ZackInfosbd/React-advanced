1ea4913c1541d6682ad91cf1cd6c73f9c60d9fdc
zackinfosbd <zackinfosbd@gmail.com>
2021-06-24 17:39:07 +0100

    first React Router  commit message

--- src/tutorial/11-react-router/setup/Navbar.js
+++ src/tutorial/11-react-router/setup/Navbar.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const Navbar = () => {
-  return <nav>navbar</nav>;
+  return <nav>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/people">People</Link>
+      </li>
+    </ul>
+  </nav>;
 };
 
 export default Navbar;
+
+/* * to navigate your links in React, avoid using the classical way like 
+  we used to do before with html but instead we import the link from our 
+  react-router-dom and add also the to="" prop to it*/
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -10,7 +10,47 @@ import Person from './Person';
 // navbar
 import Navbar from './Navbar';
 const ReactRouterSetup = () => {
-  return <h2>react router</h2>;
+  return <Router>
+    <Navbar/>
+    <Switch>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/about">
+        <About/>
+      </Route>
+      <Route path="/people">
+        <People/>
+      </Route>
+      <Route path="/person/:id" children={<Person/>}>
+        <Person/>
+      </Route>
+      <Route  path="*">
+        <Error/>
+      </Route>
+    </Switch>
+  </Router>;
 };
 
 export default ReactRouterSetup;
+
+/*
+  * to use the react routing, you need install the package.
+  * later then wrape the whole APP in Router in app.js or index.js
+  * to navigate to your exact page you need add eaxct prop to the Route of 
+  the Home Page, or you can do it on lthe level of each nested route if there 
+  will be some nested Routes
+  * for no existing routes include star(*) in the path, but that also could 
+  couses some probms with the routing.
+  * add Switch component and let all the routes be included within the switch 
+  component to solve all the missrouting
+  * Navbar component should be outside of the Switch component.
+  * URL Params & Placeholders are some sort of routing to but to pick a single
+  item from the list shown in the parent of that item component - 
+  peoples(list) - people(item), Products(list) - Product(item)
+  {
+    <Route path="/:id" children={<Person/>}> or <Route path="/person/:id" children={<Person/>}>
+        <Person/>
+      </Route>
+  }
+  */
