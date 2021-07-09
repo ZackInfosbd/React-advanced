e8161bebf8f1ecc73cbaa3b3ced43c886b86db6c
Zack DB DEV <zackinfosbd@gmail.com>
2021-07-09 16:15:02 +0100

    revision the custom Hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import SetUp from "./tutorial/6-useReducer/setup/index";
+import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
 function App() {
   return (
     <div className="container">
-      <SetUp />
+      <Setup />
     </div>
   );
 }
--- src/tutorial/10-prop-types/setup/index.js
+++ src/tutorial/10-prop-types/setup/index.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import Product from './Product'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React from "react";
+import Product from "./Product";
+import { useFetch } from "../../9-custom-hooks/final/2-useFetch";
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-prop-types-example'
+const url = "https://course-api.com/react-prop-types-example";
 
 const Index = () => {
-  const { products } = useFetch(url)
+  const { products } = useFetch(url);
   return (
     <div>
       <h2>products</h2>
-      <section className='products'>
+      <section className="products">
         {products.map((product) => {
-          return <Product key={product.id} {...product} />
+          return <Product key={product.id} {...product} />;
         })}
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
 
 /*
-  * PropTypes allow us to validate our props we are passing, means that 
+  * PropTypes allow us to validate our props we are passing into component, means that 
 some of our objects have not some given properties as the rest of the objects
      1 - that's why we need to set up some props types to check that for me
      2 - set up some default values or short-ciruit operators.
