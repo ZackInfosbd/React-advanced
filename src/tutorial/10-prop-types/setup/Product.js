bace9b3f8789ec6bdc36ff1a9402530d0260f5f3
Zack DB DEV <zackinfosbd@gmail.com>
2021-07-10 16:32:04 +0100

    revision propTypes

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import PropTypes from "prop-types"
-import defaultImage from '../../../assets/default-image.jpeg' 
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = ({image,name,price}) => {
-  const url = image && image.url 
-  console.log(image,name,price);
-  return <article className='product'>
-    {/* cause an error because one of the object does not have img proprety */}
-    <img src={url || defaultImage} alt={name || 'default name'} />
-    {/* <img src={image.url} alt={name} /> */}
-    <h4>{name} </h4>
-    <p>${price || 3.99}</p>
-    {/* check which product has no image property */}
-    {/* <h4>single product</h4> */}
-    
-  </article>;
+const Product = ({ image, name, price }) => {
+  const url = image && image.url;
+  console.log(image, name, price);
+  return (
+    <article className="product">
+      {/* cause an error because one of the object does not have img proprety */}
+      <img src={url || defaultImage} alt={name || "default name"} />
+      {/* <img src={image.url} alt={name} /> */}
+      <h4>{name} </h4>
+      <p>${price || 3.99}</p>
+      {/* check which product has no image property */}
+      {/* <h4>single product</h4> */}
+    </article>
+  );
 };
 
-// set up propTypes from the package PropTypes.
+// set up propTypes from the package PropTypes to check our props.
 Product.propTypes = {
-  image:PropTypes.object.isRequired,
-  name:PropTypes.string.isRequired,
-  price:PropTypes.number.isRequired
-}
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
 
 // set up default value/props from the package prop-types
-Product.defaultProps = {
-  name:'default name',
-  // price:3.99,
-  image: defaultImage
-}
+// Product.defaultProps = {
+//   name: "default name",
+//   price: 3.99,
+//   image: defaultImage,
+// };
+
 export default Product;
